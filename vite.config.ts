f1923c9e777d4513b9e698791ee6519093046372
Shaun Williamson <131219273+primalrockstar@users.noreply.github.com>
2025-07-09 13:17:49 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // This is the critical line that tells Vite where your frontend code lives.
+  root: 'client', 
   plugins: [react()],
+  build: {
+    // This tells Vite where to put the final built files.
+    outDir: '../dist',
+    emptyOutDir: true,
+  }
 })
