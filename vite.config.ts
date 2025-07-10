204ab387500908d964afdabb3d5b7a0ff5d1427b
Shaun Williamson <131219273+primalrockstar@users.noreply.github.com>
2025-07-09 20:57:06 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// This is the definitive configuration for a 'client' sub-directory.
 export default defineConfig({
-  root: 'client',
   plugins: [react()],
   resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './client/src'),
-    },
-  },
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-  },
+    extensions: ['.js', '.ts', '.tsx', '.json']
+  }
 })
