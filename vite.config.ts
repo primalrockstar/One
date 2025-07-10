b3411ef4f8dd1083e319b1f32582ca4dfaca8b8c
Shaun Williamson <131219273+primalrockstar@users.noreply.github.com>
2025-07-09 17:57:16 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// This is the standard, default configuration now that index.html is in the root.
+// It has no confusing path overrides.
 export default defineConfig({
   plugins: [react()],
 })
