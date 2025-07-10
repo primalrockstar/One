3bcfb05928bc1de7ed188680366e9cb12e384201
Shaun Williamson <131219273+primalrockstar@users.noreply.github.com>
2025-07-09 22:44:08 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname, 'client'), // Point to client directory
+  publicDir: path.resolve(__dirname, 'client/public'),
   build: {
-    outDir: 'dist',
-    emptyOutDir: true
+    outDir: path.resolve(__dirname, 'dist'),
+    emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, 'client/index.html')
+    }
   },
-  esbuild: {
-    jsxInject: `import React from 'react'`
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'client/src')
+    }
   }
 })
